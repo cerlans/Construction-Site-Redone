37ebf9b55866b1f595134aa51ecec87dd40bb236
cerlans <kgomez222@hotmail.com>
2021-02-10 15:42:21 -0500

    4 files modified, 1 added

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Gallery from './Gallery.js'
 function Content (){
   return(
     <>
---
+++ src/Gallery.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Gallery(){
+  return (
+    <>
+     <div className='card'>
+        <div className='icon'>
+        <i class="far fa-building"></i>
+        <h3>Construction Services</h3>
+        </div>
+        <div className='content'>
+        <p>We provide high-quality construction services to a wide variety of clients.</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Gallery;
