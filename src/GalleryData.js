ca636a0ab3ad1e98720648965fd3f648ae22d46c
cerlans <kgomez222@hotmail.com>
2021-02-18 13:19:08 -0500

    1 file modified

--- src/GalleryData.js
+++ src/GalleryData.js
@@ -3,10 +3,14 @@
 const data = [{
   image:'https://i.vgy.me/WFgC1K.jpg',
   address:'80 Devonshire Ave. '
-},{image:'https://i.vgy.me/WFgC1K.jpg',
-  address:'598 Berkshire St.  '},{image:'https://i.vgy.me/WFgC1K.jpg',
-  address:'496 Grand St.'},{image:'https://i.vgy.me/WFgC1K.jpg',
-  address:'8050 Ohio Rd.  '}]
+},
+{image:'https://images.unsplash.com/photo-1520528938231-ca6d3d2f9608?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJ1aWxkaW5nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+  address:'598 Berkshire St.  '
+  },
+  {image:'https://images.unsplash.com/photo-1512864733469-8c0d7cc3ccf5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80',
+  address:'496 Grand St.'},
+  {image:'https://images.unsplash.com/photo-1554797589-7241bb691973?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80',
+  address:'8050 Ohio Rd'}]
 
 
   export default data
