c7888ae0c1880944519ef6a1f3332f025107105e
cerlans <kgomez222@hotmail.com>
2021-02-23 12:22:06 -0500

    2 files modified

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import data from './GalleryData.js';
 import Card from './Card.js'
+
 function Gallery(){
   const [filter,setFilter] = useState('')
   const [myData,setData] = useState()
- 
+  const [currentClass,setClass] = useState('hidden-menu')
+
   useEffect(()=> {
     if(filter === ''){
      let allResults = data.map(function(value){
@@ -15,15 +17,42 @@ function Gallery(){
       let filteredResult = data.filter(function (value) { 
         return value.tag === filter; 
     }).map(function (value) { 
-        return <Clipboard tag = {value.tag} photo = {value.photo} address = {value.address} />
+        return <Card tag = {value.tag} photo = {value.photo} address = {value.address} />
     })
     setData(filteredResult) 
     }
   },[filter])
   return (
     <>
-  <h1>Some More Stuff</h1>
- <button onClick = {function(){
+    <div className='filter-container'>
+      <div className='filter-button' onClick={()=>{ currentClass ? setClass('filter-menu'): setClass('hidden-menu')} }>
+       Filter
+      <i class="fas fa-sort-down"></i>
+        <div className={currentClass}>
+          <button onClick = {function(){
+       setFilter('')}}> See ALL photos</button>
+ <button onClick={function(){
+   setFilter('Nature')
+ }}>nature photos</button>
+  <button onClick={function(){
+   setFilter('Commercial')
+ }}>Commercial photos</button>
+  <button onClick={function(){
+   setFilter('Residential')
+ }}>residential photos</button>
+        </div>
+      </div>
+    </div>
+ 
+  {myData}
+    </>
+  )
+}
+
+export default Gallery;
+
+
+/* <button onClick = {function(){
    setFilter('')
  }}> See ALL photos</button>
  <button onClick={function(){
@@ -35,9 +64,4 @@ function Gallery(){
   <button onClick={function(){
    setFilter('Residential')
  }}>All residential photos</button>
-  {myData}
-    </>
-  )
-}
-
-export default Gallery;
+ */
