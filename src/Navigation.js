188c3a5ad470afb3993335a76aceff3cd732eabb
cerlans <kgomez222@hotmail.com>
2021-02-23 22:46:35 -0500

    3 files modified

--- src/Navigation.js
+++ src/Navigation.js
@@ -9,8 +9,8 @@ function Navigation (){
     </nav>
     <section className='Hero'>
     <div className ='Hero-content'>
-    <h1>STAINLESS REPUTATION</h1>
-    <p>We've earned an outstand reputation for our quality work and our integrity in dealing with clients,architects,suppliers,and sub-contractors.</p>
+    <h1 className='animate__animated animate__fadeInLeft'>STAINLESS REPUTATION</h1>
+    <p className='animate__animated  animate__fadeInRight'>We've earned an outstand reputation for our quality work and our integrity in dealing with clients,architects,suppliers,and sub-contractors.</p>
     <button className='hvr-fade'>Learn more</button>
     </div>
     </section>
