36669ba6d59606508b82322aa850884f5aead9bd
cerlans <kgomez222@hotmail.com>
2021-02-23 22:54:45 -0500

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.css";
 import Navigation from './Navigation.js'
 import Content from './Content.js'
+import Footer from './Footer.js'
 export default function App() {
   return (
     <>
       <Navigation/>
       <Content />
+      <Footer/>
     </>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Footer(){
+  return (
+    <>
+      <div className='footer'></div>
+    </>
+  )
+}
+
+export default Footer;
