a98edebefbdb8231a0ce3bafce0b16dd97e882a8
cerlans <kgomez222@hotmail.com>
2021-02-26 14:57:50 -0500

    2 files modified

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navigation (){
+    const [count, setCount] = useState(0);
   return (
     <>
     <nav className='navigation'>
@@ -12,6 +13,15 @@ function Navigation (){
     <h1 className='animate__animated animate__fadeInLeft'>STAINLESS REPUTATION</h1>
     <p className='animate__animated  animate__fadeInRight'>We've earned an outstand reputation for our quality work and our integrity in dealing with clients,architects,suppliers,and sub-contractors.</p>
     <button className='hvr-fade'>Learn more</button>
+    <div className='slides-btn'>
+    <i class="fas fa-circle" onClick={()=>{
+      setCount(2)
+    }}></i>
+    <i class="fas fa-circle" onClick={()=>{
+      console.log(count)
+    }}></i>
+    <i class="fas fa-circle"></i>
+    </div>
     </div>
     </section>
     </>
