b17fa4f5d546988737277289a39faa49edd10ee3
cerlans <kgomez222@hotmail.com>
2021-03-24 14:22:52 -0400

    2 files modified

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 
 function Navigation() {
-  const [count, setCount] = useState(0);
-  let data = [
-    "https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    "https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
-    "https://images.unsplash.com/photo-1454694220579-9d6672b1ec2a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60",
-  ];
   return (
     <>
       <nav className="navigation">
@@ -33,10 +27,7 @@ function Navigation() {
           </li>
         </ul>
       </nav>
-      <section
-        className="Hero"
-        style={{ backgroundImage: `url(${data[count]})` }}
-      >
+      <section className="Hero">
         <div className="Hero-content">
           <h1 className="animate__animated animate__fadeInLeft">
             STAINLESS REPUTATION
