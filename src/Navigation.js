6445bc88e2875086a866fd9d0c5b9663da5f554b
cerlans <kgomez222@hotmail.com>
2021-04-17 13:07:56 -0400

    2 files modified

--- src/Navigation.js
+++ src/Navigation.js
@@ -9,8 +9,8 @@ function Navigation() {
             <span className="logo">Lex</span> contractors
           </h1>
         </div>
-        <div className='mobile'>
-        <i class="fas fa-bars"></i>
+        <div className="mobile">
+          <i class="fas fa-bars"></i>
         </div>
         <ul className="desktop-nav">
           <li class="hvr-underline-from-center">Home</li>
@@ -29,39 +29,9 @@ function Navigation() {
       </nav>
       <section className="Hero">
         <div className="Hero-content">
-          <h1 className="animate__animated animate__fadeInLeft">
-            STAINLESS REPUTATION
-          </h1>
-          <p className="animate__animated  animate__fadeInRight">
-            At our company, we hire the best and the most committed construction experts who move the industry forward and create great spaces for your life and work
-          </p>
-          <button
-            className="hvr-fade"
-            className="animate__animated animate__fadeInUp"
-          >
-            Learn more
-          </button>
-          <div className="slides-btn">
-            <i
-              class="fas fa-circle"
-              onClick={() => {
-                setCount(0);
-              }}
-            ></i>
-            <i
-              class="fas fa-circle"
-              onClick={() => {
-                setCount(1);
-              }}
-            ></i>
-            <i
-              class="fas fa-circle"
-              onClick={() => {
-                setCount(2);
-              }}
-            ></i>
+          <div className="hero-Image">
+            <img src="https://i.vgy.me/OmzKhK.jpg" alt="building" />
           </div>
-          
         </div>
       </section>
     </>
